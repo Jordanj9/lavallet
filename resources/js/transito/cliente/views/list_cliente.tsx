00242428edeb4c00adcc318662a8510788e1fed2
colonca <c_colon10@hotmail.com>
2021-03-26 09:44:24 -0500

    LAV-25 Registrar Cliente

--- resources/js/transito/cliente/views/list_cliente.tsx
+++ resources/js/transito/cliente/views/list_cliente.tsx
@@ -62,7 +62,7 @@ const ListClientes: React.FC = () => {
       <ModalClienteEliminar show={showEliminar} hidden={hiddenModal} id={id} service={service}/>
       <div className='flex justify-end'>
         <button onClick={() => { showModal(); }} 
-          className='p-2 px-4 inline-flex text-xs leading-5 font-semibold rounded-lg text-green-50'
+          className='p-2 px-4 inline-flex text-xs leading-5 font-bold rounded-lg text-green-50'
           style={{backgroundColor: '#45BF55'}}>
           <svg 
             className='w-6 h-6 text-white-500'
@@ -76,6 +76,7 @@ const ListClientes: React.FC = () => {
               strokeWidth={2} 
               d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
           </svg>
+          <span className="ml-2">Agregar</span>
         </button>
       </div>
     </div>
