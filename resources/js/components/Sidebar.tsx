8c45dc8e530e51a11b3a1ca3c1e25f3d1ed272a6
colonca <c_colon10@hotmail.com>
2021-04-16 12:26:40 -0500

    LAV-4 Generar Vale

--- resources/js/components/Sidebar.tsx
+++ resources/js/components/Sidebar.tsx
@@ -63,17 +63,21 @@ const Sidebar = () => {
           <li id="transito">
             <div className="p-2 flex items-cente">
               <svg
-                className="h-6 w-6 -mt-1"
+                className="h-6 w-6 -mt-1 "
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
                 <path
+                  fill="#fff"
+                  d="M9 17a2 2 0 11-4 0 2 2 0 014 0zM19 17a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+                <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                  strokeWidth={2}
+                  d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"
                 />
               </svg>
               <Link to="/transito/entrada">Transito</Link>
